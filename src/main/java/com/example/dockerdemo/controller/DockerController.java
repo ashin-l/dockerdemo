a03f547aaf0cf6ba5affad30781f8146676ba111
lqc <lqc.ashin@gmail.com>
2020-04-24 14:20:32 +0800

    fc

---
+++ src/main/java/com/example/dockerdemo/controller/DockerController.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "Hello docker!";
+  }
+}
